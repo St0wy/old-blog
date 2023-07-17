2384fb5fa96560d7d4e2284aabeeed1b18e8e6c4
Stowy <stowy@protonmail.ch>
2023-07-17 22:50:42 +0200

    Fixed video tag and og image

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -2,17 +2,19 @@ import { getCollection } from "astro:content";
 import generateOgImage from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 
+const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
+const posts = Object.values(postImportResult);
+
 export const get: APIRoute = async ({ params }) => ({
   body: await generateOgImage(params.ogTitle),
 });
-
-const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
-const posts = Object.values(postImportResult);
 
 export function getStaticPaths() {
   return posts
     .filter(({ data }) => !data.ogImage)
     .map(({ data }) => ({
-      params: { ogTitle: data.title },
+      params: { 
+        ogTitle: data.postSlug ? data.postSlug : data.title,
+      },
     }));
 }
