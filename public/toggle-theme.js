1f1c2264a679b3583dbe2a120661ffeb961b879e
Stowy <stowy@protonmail.ch>
2023-09-08 15:42:35 +0200

    Added view transition API

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,52 +1,64 @@
-const primaryColorScheme = ""; // "light" | "dark"
+if (!window.ThemeGlobal) {
+  console.log("Test");
+  window.ThemeGlobal = {};
 
-// Get theme data from local storage
-const currentTheme = localStorage.getItem("theme");
+  const primaryColorScheme = ""; // "light" | "dark"
 
-function getPreferTheme() {
-  // return theme value in local storage if it is set
-  if (currentTheme) return currentTheme;
+  // Get theme data from local storage
+  const currentTheme = localStorage.getItem("theme");
 
-  // return primary color scheme if it is set
-  if (primaryColorScheme) return primaryColorScheme;
+  function getPreferTheme() {
+    // return theme value in local storage if it is set
+    if (currentTheme) return currentTheme;
 
-  // return user device's prefer color scheme
-  return window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
-}
+    // return primary color scheme if it is set
+    if (primaryColorScheme) return primaryColorScheme;
 
-let themeValue = getPreferTheme();
+    // return user device's prefer color scheme
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
 
-function setPreference() {
-  localStorage.setItem("theme", themeValue);
+  let themeValue = getPreferTheme();
+
+  function setPreference() {
+    localStorage.setItem("theme", themeValue);
+    reflectPreference();
+  }
+
+  function reflectPreference() {
+    document.firstElementChild.setAttribute("data-theme", themeValue);
+
+    document
+      .querySelector("#theme-btn")
+      ?.setAttribute("aria-label", themeValue);
+  }
+
+  // set early so no page flashes / CSS is made aware
   reflectPreference();
+
+  document.addEventListener(
+    "astro:page-load",
+    () => {
+      // set on load so screen readers can get the latest value on the button
+      reflectPreference();
+      console.log("Test");
+
+      // now this script can find and listen for clicks on the control
+      document.querySelector("#theme-btn")?.addEventListener("click", () => {
+        themeValue = themeValue === "light" ? "dark" : "light";
+        setPreference();
+      });
+    },
+    { once: true }
+  );
+
+  // sync with system changes
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", ({ matches: isDark }) => {
+      themeValue = isDark ? "dark" : "light";
+      setPreference();
+    });
 }
-
-function reflectPreference() {
-  document.firstElementChild.setAttribute("data-theme", themeValue);
-
-  document.querySelector("#theme-btn")?.setAttribute("aria-label", themeValue);
-}
-
-// set early so no page flashes / CSS is made aware
-reflectPreference();
-
-window.onload = () => {
-  // set on load so screen readers can get the latest value on the button
-  reflectPreference();
-
-  // now this script can find and listen for clicks on the control
-  document.querySelector("#theme-btn")?.addEventListener("click", () => {
-    themeValue = themeValue === "light" ? "dark" : "light";
-    setPreference();
-  });
-};
-
-// sync with system changes
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", ({ matches: isDark }) => {
-    themeValue = isDark ? "dark" : "light";
-    setPreference();
-  });
