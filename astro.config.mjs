a308f609e5e1a63056c036d441eee60f138a195d
Stowy <stowy@protonmail.ch>
2023-09-26 15:32:43 +0200

    Added build systems blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,51 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import { SITE } from "./src/config";
+import { readFileSync } from "fs";
+
+const nushellGrammar = JSON.parse(
+	readFileSync("./src/utils/nushell.tmLanguage.json")
+);
+const nushellLanguage = {
+	id: "nushell",
+	scopeName: "source.nushell",
+	grammar: nushellGrammar,
+	aliases: ["nu"],
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), sitemap(), mdx()],
-  markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }]],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true
-    },
-    extendDefaultPlugins: true
-  },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+	site: SITE.website,
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		react(),
+		sitemap(),
+		mdx(),
+	],
+	markdown: {
+		remarkPlugins: [
+			remarkToc,
+			[
+				remarkCollapse,
+				{
+					test: "Table of contents",
+				},
+			],
+		],
+		shikiConfig: {
+			theme: "one-dark-pro",
+			wrap: true,
+			langs: [nushellLanguage],
+		},
+		extendDefaultPlugins: true,
+	},
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
