e980f0188acd2e10d0daf50fcab058153a0b3ff2
Stowy <stowy@protonmail.ch>
2023-11-25 15:30:26 +0100

    Fixed syntax highlighting and removed twitter from socials

--- src/config.ts
+++ src/config.ts
@@ -1,61 +1,61 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://blog.stowy.ch",
-  author: "Stowy",
-  desc: "The game dev blog of Stowy.",
-  title: "Stowy's Blog",
-  ogImage: "",
-  lightAndDarkMode: true,
-  postPerPage: 4,
-  comments: {
-    host: "mastodon.gamedev.place",
-    username: "@stowy",
-  }
+	website: "https://blog.stowy.ch",
+	author: "Stowy",
+	desc: "The game dev blog of Stowy.",
+	title: "Stowy's Blog",
+	ogImage: "",
+	lightAndDarkMode: true,
+	postPerPage: 4,
+	comments: {
+		host: "mastodon.gamedev.place",
+		username: "@stowy",
+	}
 };
 
 export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
+	enable: false,
+	svg: true,
+	width: 216,
+	height: 46,
 };
 
 export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/St0wy",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: "GitLab",
-    href: "https://gitlab.com/Stowy",
-    linkTitle: `${SITE.title} on GitLab`,
-    active: true,
-  },
-  {
-    name: "Mastodon",
-    href: "https://mastodon.gamedev.place/@stowy",
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: true,
-  },
-  {
-    name: "Twitter",
-    href: "https://twitter.com/St0wy",
-    linkTitle: `${SITE.title} on Twitter`,
-    active: true,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/fabian-huber-530b24207/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:stowy@protonmail.ch",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: true,
-  }
+	{
+		name: "Github",
+		href: "https://github.com/St0wy",
+		linkTitle: ` ${SITE.title} on Github`,
+		active: true,
+	},
+	{
+		name: "GitLab",
+		href: "https://gitlab.com/Stowy",
+		linkTitle: `${SITE.title} on GitLab`,
+		active: true,
+	},
+	{
+		name: "Mastodon",
+		href: "https://mastodon.gamedev.place/@stowy",
+		linkTitle: `${SITE.title} on Mastodon`,
+		active: true,
+	},
+	{
+		name: "Twitter",
+		href: "https://twitter.com/St0wy",
+		linkTitle: `${SITE.title} on Twitter`,
+		active: false,
+	},
+	{
+		name: "LinkedIn",
+		href: "https://www.linkedin.com/in/fabian-huber-530b24207/",
+		linkTitle: `${SITE.title} on LinkedIn`,
+		active: true,
+	},
+	{
+		name: "Mail",
+		href: "mailto:stowy@protonmail.ch",
+		linkTitle: `Send an email to ${SITE.title}`,
+		active: true,
+	}
 ];
